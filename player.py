f45105668ae03e3757600e7889b4069dcf52cdaf
Alex Koumparos <crossroadsman@gmail.com>
2017-11-23 19:04:39 -0700

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,22 @@
+from enum import Enum
 
 
 class Player:
 
-    def move(self, direction: str):
-        print("moving in direction: {}".format(direction))
+    def move(self, direction: Direction):
+        print("moving in direction: {}".format(direction.value))
+        
+    # the following is just an example of type hinting in practice
+    # see PEP484 for more details
+    def plusOne(self, value: int) -> int:
+        return value + 1
+
+
+class Direction(Enum):
+    # syntax is MEMBER_NAME = value
+    # name can be accessed using <thing>.name
+    # value can be accessed using <thing>.value
+    NORTH = "north"
+    EAST = "east"
+    SOUTH = "south"
+    WEST = "west"
